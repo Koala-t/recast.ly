19ae444938b00e8aa4d8a9bfb80d69ca6f5428fc
Koala-t <johnsonc.88@gmail.com>
2016-03-18 16:19:51 -0700

    Display the correct titles and descriptions for videos in the list

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -4,8 +4,8 @@ var VideoListEntry = (props) => (
       <img className="media-object" src={props.datum.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{props.datum.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.datum.snippet.description}</div>
     </div>
   </div>
 );
