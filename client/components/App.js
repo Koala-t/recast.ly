fd7b5acbd4cd61e545ae25fd92443eec1138390a
Koala-t <johnsonc.88@gmail.com>
2016-03-18 16:17:06 -0700

    Display thumbnails for example videos

--- client/components/App.js
+++ client/components/App.js
@@ -2,12 +2,13 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer />
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList exampleVideoData={exampleVideoData}/>
     </div>
   </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ 
