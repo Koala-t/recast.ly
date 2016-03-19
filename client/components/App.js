0c6dc17503a1f7f9faa23dd2a499bfd58139e15f
Koala-t <johnsonc.88@gmail.com>
2016-03-18 19:33:20 -0700

    Update video src from exampleVideoData, refactor App.js into class

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,18 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer />
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer startVideo={exampleVideoData[0]}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList exampleVideoData={exampleVideoData}/>
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList exampleVideoData={exampleVideoData}/>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
  
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,11 +1,13 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
+  // {var address = "https://www.youtube.com/embed/" + {props.startVideo.id.videoId} + "?autoplay=1"}
+
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.startVideo.id.videoId + "?autoplay=1"} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.startVideo.snippet.title}</h3>
+      <div>{props.startVideo.snippet.description}</div>
     </div>
   </div>
 );
